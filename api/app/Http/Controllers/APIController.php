82f4e5d4a52b9adfab27410bdd39550c9cdcf93f
Call Vin <kanggara75@gmail.com>
2022-06-28 15:55:18 +0700

    add: login with POST Req #api

--- api/app/Http/Controllers/APIController.php
+++ api/app/Http/Controllers/APIController.php
@@ -14,7 +14,24 @@ class APIController extends Controller
 
 	public function login(Request $request)
 	{
-		return response()->json(["msg" => "OK"], 201, [], JSON_NUMERIC_CHECK);
+		$uid = $request->input('uid');
+
+		$cek = Plate::where('uid', "=", $uid)->first();
+
+		if ($cek) {
+			return response()->json(["msg" => "Not OK"], 409, [], JSON_NUMERIC_CHECK);
+		}
+
+		if (!$uid) {
+			return response()->json(["msg" => "Not OK"], 400, [], JSON_NUMERIC_CHECK);
+		}
+		$plate = new Plate;
+		$plate->uid = $uid;
+		$plate->status = 1;
+		$plate->save();
+
+		$data = Plate::where('uid', "=", $uid)->first();
+		return response()->json($data, 201, [], JSON_NUMERIC_CHECK);
 	}
 
 	public function get(Request $request)
