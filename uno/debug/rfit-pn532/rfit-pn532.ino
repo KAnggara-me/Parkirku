5f24d685227de8aae9cabc80c6f0add9eca762b7
Call Vin <kanggara75@gmail.com>
2022-07-02 13:54:07 +0700

    add: rfid PN352 debud test

---
+++ uno/debug/rfit-pn532/rfit-pn532.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <Adafruit_PN532.h>
+
+#define PN532_IRQ   (4)
+#define PN532_RESET (15)  // Not connected by default on the NFC Shield
+
+// Or use this line for a breakout or shield with an I2C connection:
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+String idcard;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) delay(10); // for Leonardo/Micro/Zero
+
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+
+  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    Serial.println("Found an ISO14443A card");
+    // Display some basic information about the card
+    idcard = "";
+    for (byte i = 0; i <= uidLength - 1; i++) {
+      idcard += (uid[i] < 0x10 ? "0" : "") +
+                String(uid[i], HEX);
+    }
+    Serial.print("ID CARD : ");
+    Serial.print(idcard);
+    Serial.println("");
+  }
+
+  delay(1000);
+}
