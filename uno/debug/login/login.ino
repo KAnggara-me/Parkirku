5cf88be0118cbe16da9400a0b2ae75e269d3a062
Call Vin <kanggara75@gmail.com>
2022-07-02 14:23:15 +0700

    add: Login post method

---
+++ uno/debug/login/login.ino
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Kos_oren";
+const char* password = "masihyanglama";
+
+//Your Domain name with URL path or IP address with path
+String serverName = "http://parkirku.apiwa.tech/api/login";
+
+unsigned long lastTime = 0;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED) {
+      WiFiClient client;
+      HTTPClient http;
+
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverName);
+
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // Data to send with HTTP POST
+      String httpRequestData = "uid=abcdef";
+      // Send HTTP POST request
+      int httpResponseCode = http.POST(httpRequestData);
+
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+      if ((httpResponseCode >= 200 ) && (httpResponseCode < 300))
+      {
+        digitalWrite(LED_BUILTIN, HIGH);
+      } else {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
