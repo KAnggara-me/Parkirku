5326b34055f1007e5317badb68e675a94026f6df
Call Vin <kanggara75@gmail.com>
2022-07-19 15:45:51 +0700

    fix: CI/CD #api

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,18 +5,14 @@ use App\Http\Controllers\APIController;
 
 Route::get("/", APIController::class . "@index");
 Route::get("/", [APIController::class, "index"]);
-
 Route::get("/uid", [APIController::class, "get"]);
+Route::get("/last", [APIController::class, "getLast"]);
+Route::get("/keluar", [APIController::class, "keluar"]);
 Route::get("/uid/{id}", [APIController::class, "getById"]);
 
 Route::post("/login",  [APIController::class, "login"]);
-
+Route::post("/update", [APIController::class, "update"]);
 Route::post("/logout",  [APIController::class, "logout"]);
+Route::post("/update2", [APIController::class, "update2"]);
 
 Route::delete("/delete", [APIController::class, "delete"]);
-
-Route::get("/last", [APIController::class, "getLast"]);
-
-Route::post("/update", [APIController::class, "update"]);
-Route::get("/keluar", [APIController::class, "keluar"]);
-Route::post("/update2", [APIController::class, "update2"]);
